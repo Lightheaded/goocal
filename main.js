a9c4bdf1b79f7f6d5b0e76a4e7a3b558e3dc38c7
Toomas Välja <toomas.valja@fortumo.com>
2017-10-12 08:58:44 +0300

    Don't unload the browser window on close, so startup would be faster.
    Proof of concept has proven to be useful. However every time the calendar
    is opened it's loaded all over again. Tried to not unload it upon closing
    but just hide it.
    
    Summary:
    - Use a webview instead of just navigating
    - Add a Cmd+W shortcut to close the window
    - TODO: Allow quitting

--- main.js
+++ main.js
@@ -2,57 +2,79 @@
 
 const {app, BrowserWindow, Menu} = require('electron');
 
-const path = require('path');
 const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
+exports.hideWindow = () => {
+  mainWindow.hide();
+};
+
+function toggleDevTools() {
+  mainWindow.webContents.toggleDevTools();
+}
+
 function createMenus() {
   const template = [{
-    label: "Application",
+    label: 'Application',
     submenu: [
-      { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
-      { type: "separator" },
-      { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
-    ]}, {
-    label: "Edit",
+      {label: 'About Application', selector: 'orderFrontStandardAboutPanel:'},
+      {type: 'separator'},
+      {
+        label: 'Close', accelerator: 'Command+W', click: () => mainWindow.close()
+      },
+      {
+        label: 'Quit', accelerator: 'Command+Q', click: () => app.quit()
+      }
+    ]
+  }, {
+    label: 'Edit',
     submenu: [
-      { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-      { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-      { type: "separator" },
-      { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-      { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-    ]}
-  ];
+      {label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:'},
+      {label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:'},
+      {type: 'separator'},
+      {label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:'},
+      {label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:'},
+      {label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:'},
+      {label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:'}
+    ]
+  }, {
+    label: 'Advanced',
+    submenu: [
+      {label: 'Show console', accelerator: 'CmdOrCtrl+J', click: toggleDevTools}
+    ]
+  }];
   Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 }
 
 function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({ width: 1200, height: 800 });
+  mainWindow = new BrowserWindow({width: 1200, height: 800});
 
   // and load the index.html of the app.
   mainWindow.loadURL(
-    url.format({
-      pathname: 'calendar.google.com',
-      protocol: 'https:',
-      slashes: true,
-    })
+      url.format({
+        pathname: `${__dirname}/index.html`,
+        protocol: 'file:',
+        slashes: true
+      })
   );
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools();
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null;
+  });
+
+  mainWindow.on('close', event => {
+    event.preventDefault();
+    mainWindow.hide();
   });
 
   createMenus();
@@ -64,7 +86,7 @@ function createWindow() {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
@@ -72,11 +94,13 @@ app.on('window-all-closed', function() {
   }
 });
 
-app.on('activate', function() {
+app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow();
+  } else {
+    mainWindow.show();
   }
 });
 
