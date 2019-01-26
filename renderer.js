5a3ba597d35442b046d8b7f28b4cc28213def60a
Toomas Välja <toomas.valja@fortumo.com>
2019-01-26 22:03:34 +0200

    Update to electron 4
    Took 12 minutes

--- renderer.js
+++ renderer.js
@@ -1,3 +1,22 @@
-// const {remote} = require('electron');
+const { remote, shell } = require('electron')
 // const main = remote.require('./main.js');
 
+function configureOpenRenderedLinksInDefaultBrowser() {
+  const aAll = document.querySelectorAll('a')
+  if (aAll && aAll.length) {
+    aAll.forEach(function (a) {
+      console.log('link', a);
+      a.addEventListener('click', function (event) {
+        if (event.target) {
+          event.preventDefault()
+          let link = event.target.href
+          shell.openExternal(link)
+        }
+      })
+    })
+  }
+}
+
+setTimeout(function () {
+  configureOpenRenderedLinksInDefaultBrowser()
+}, 1000)
